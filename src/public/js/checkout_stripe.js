44478f302b3de9fe072f8ff407ae54fe3b29f3c9
xavigrausebastian <xavier.grau@kreatings.com>
2022-05-05 13:42:03 +0200

    Add validation to inputs

--- src/public/js/checkout_stripe.js
+++ src/public/js/checkout_stripe.js
@@ -22,6 +22,9 @@ function addEventListener() {
 	header_2.addEventListener('click', toStep2);
 	header_3.addEventListener('click', toStep3);
 
+	// Key up validations
+	onKeyupInputValidation();
+
 }
 
 function toStep1() {
@@ -46,6 +49,10 @@ function toStep1() {
 
 function toStep2() {
 
+	if (!isValidContactForm()) {
+		return;
+	}
+
 	const step1 = document.getElementById('step__1');
 	const step2 = document.getElementById('step__2');
 	const step3 = document.getElementById('step__3');
@@ -66,6 +73,10 @@ function toStep2() {
 
 function toStep3() {
 
+	if (!isValidContactForm()) {
+		return;
+	}
+
 	const step1 = document.getElementById('step__1');
 	const step2 = document.getElementById('step__2');
 	const step3 = document.getElementById('step__3');
@@ -80,6 +91,91 @@ function toStep3() {
 
 	if (step3.classList.contains('d-none')) {
 		step3.classList.remove('d-none');
+	}
+
+}
+
+function isValidContactForm() {
+
+	const inputs = document.getElementsByClassName('input__required');
+	let valid = true;
+
+	for (let i = 0; i < inputs.length; i++) {
+
+		let input = inputs[i];
+		let input_error_message = input.nextElementSibling;
+
+		if (input.value === "") {
+
+			valid = false;
+
+			if (!input.classList.contains('input__error')) {
+				input.classList.add('input__error');
+			}
+
+			if (input_error_message.classList.contains('d-none')) {
+				input_error_message.classList.remove('d-none');
+			}
+
+		} else {
+
+			if (input.classList.contains('input__error')) {
+				input.classList.remove('input__error');
+			}
+
+			if (!input_error_message.classList.contains('d-none')) {
+				input_error_message.classList.add('d-none');
+			}
+
+		}
+
+	}
+
+	// Auto scroll to the fist error
+	if (!valid) {
+		document.querySelector('.input__error').scrollIntoView({
+			behavior: 'smooth'
+		});
+	}
+
+	return valid;
+
+}
+
+function onKeyupInputValidation() {
+
+	const inputs = document.getElementsByClassName('input__required');
+
+	for (let i = 0; i < inputs.length; i++) {
+
+		let input = inputs[i];
+		let input_error_message = input.nextElementSibling;
+
+		input.addEventListener("keyup", () => {
+
+			if (input.value === "") {
+
+				if (!input.classList.contains('input__error')) {
+					input.classList.add('input__error');
+				}
+
+				if (input_error_message.classList.contains('d-none')) {
+					input_error_message.classList.remove('d-none');
+				}
+
+			} else {
+
+				if (input.classList.contains('input__error')) {
+					input.classList.remove('input__error');
+				}
+
+				if (!input_error_message.classList.contains('d-none')) {
+					input_error_message.classList.add('d-none');
+				}
+
+			}
+		});
+
 	}
 
 }
